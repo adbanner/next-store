4c1a4b56fd86bebfa7043a34a7b7a8e816aae227
adbanner <flash@mavi.com.ua>
2024-05-05 17:12:32 +0100

    ts6

--- lib/StoreProvider.tsx
+++ lib/StoreProvider.tsx
@@ -3,6 +3,6 @@
 import { store } from "./store";
 import { Provider } from "react-redux";
 
-export default function StoreProvider({ children }) {
+export default function StoreProvider({ children }: { children: React.ReactNode }) {
   return <Provider store={store}>{children}</Provider>;
 }
